a41206a82c8861b8f8134e6c26bb9d66065c219b
FerMarLey <FerMarcelin>
2024-12-16 10:23:57 -0600

    changing function body

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ import time
 
 st.header('Lanzar una moneda')
 
-chart =st.line_chart([0.5])
+chart = st.line_chart([0.5])
 
 def toss_coin(n):
+
     trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
 
     mean = None
@@ -14,7 +15,7 @@ def toss_coin(n):
     outcome_1_count = 0
 
     for r in trial_outcomes:
-        outcome_no += 1
+        outcome_no +=1
         if r == 1:
             outcome_1_count += 1
         mean = outcome_1_count / outcome_no
@@ -29,5 +30,3 @@ start_button = st.button('Ejecutar')
 if start_button:
     st.write(f'Experimento con {number_of_trials} intentos en curso.')
     mean = toss_coin(number_of_trials)
-
-st.write('Esta aplicación aún no es funcional. En construcción.')
